48e54013b4210cadda1886ff409f19e4769213cb
vutiendung23092002 <vutiendungdev@gmail.com>
2023-11-21 22:07:26 +0700

    add product to file product.txt

--- main.py
+++ main.py
@@ -4,18 +4,27 @@ import datetime
 def display_menu():
     print("\nMENU:")
     print("1. Thêm hàng hoá")
-    print("2. Hiển thị danh sách hàng hoá")
-    print("3. Thoát")
+    print("2. Tìm kiếm hàng hoá")
+    print("3. Hiển thị danh sách hàng hoá")
+    print("0. Thoát")
 
     choice = int(input("Nhập lựa chọn của bạn: "))
     if choice == 1:
-        print("_________________________________________________")
+        print("____________________________________________________________________________________________________")
         add_product(manager)
+        print("____________________________________________________________________________________________________")
         display_menu()
     elif choice == 2:
-        display_products(manager)
+        print("____________________________________________________________________________________________________")
+        search_by_name(manager)
+        print("____________________________________________________________________________________________________")
         display_menu()
     elif choice == 3:
+        print("____________________________________________________________________________________________________")
+        display_products(manager)
+        print("____________________________________________________________________________________________________")
+        display_menu()
+    elif choice == 0:
         print("Đã thoát.")
         exit()
     else:
@@ -32,23 +41,29 @@ def add_product(manager):
             quantity = int(input("Nhập số lượng: "))
             manufacture_date = input("Nhập ngày sản xuất (dd/mm/yyyy): ")
             expiration_date = input("Nhập hạn sử dụng (dd/mm/yyyy): ")
-
+            #Kiểm tra xem id có trùng không
+            if manager.search_by_code(product_code) is True:
+                raise ValueError(f"Sản phẩm {product_code} đã tồn tại")
             # Kiểm tra số lượng, giá nhập, giá bán > 0
             if quantity <= 0 or selling_price <= 0 or cost_price <= 0:
                 raise ValueError("Số lượng và giá tiền phải lớn hơn 0.")
-
             # Kiểm tra định dạng ngày tháng
             datetime.datetime.strptime(manufacture_date, '%d/%m/%Y')
             datetime.datetime.strptime(expiration_date, '%d/%m/%Y')
-
             manager.add_product(product_code, product_name, selling_price, cost_price, quantity, manufacture_date, expiration_date)
+            with open('list_product.txt', 'a', encoding='utf-8') as file:
+                file.write(f"{product_code},{product_name},{selling_price},{cost_price},{quantity},{manufacture_date},{expiration_date}\n")
             print("Hàng hoá đã được thêm thành công!")
-            break  # Thoát vòng lặp nếu đã thêm hàng hoá thành công
+            break
         except ValueError as e:
             print(f"Lỗi: {e}")
 
 def display_products(manager):
     manager.display_all_products()
+
+def search_by_name(manager):
+    keyword = str(input("Nhập tên sản phẩm cần tìm kiếm: "))
+    manager.search_by_name(keyword)
 
 manager = ManageProduct()
 display_menu()
--- manageProduct.py
+++ manageProduct.py
@@ -2,14 +2,58 @@ from product import Product
 
 class ManageProduct:
     def __init__(self):
-        self.product_list = [] 
+        self.product_add = []
+        self.product_show = []
+
+    def load_products_from_file(self):
+        try:
+            with open('list_product.txt', 'r', encoding='utf-8') as file:
+                for line in file:
+                    product_info = line.strip().split(',')
+                    product = Product(product_info[0], product_info[1], float(product_info[2]), float(product_info[3]), int(product_info[4]), product_info[5], product_info[6])
+                    self.product_show = []
+                    self.product_show.append(product)
+        except FileNotFoundError:
+            print("File list_product.txt không tồn tại.")
+
+    def display_products(self, products):
+        row1 = '+ {:-<5} + {:-<15} + {:-<10} + {:-<10} + {:-<10} + {:-<10} + {:-<10}  +'.format('', '', '', '', '', '', '')
+        row2 = '+ {:<5} + {:<15} + {:<10} + {:<10} + {:<10} + {:<10} + {:<10}  +'.format('MHH', 'Tên hàng hoá', 'Số lượng', 'Giá bán', 'Giá nhập', 'NSX', 'HSD')
+        print("Danh sách hàng hoá:")
+        print(row1)
+        print(row2)
+        print(row1)
+        for product in products:
+            row = '+ {:<5} + {:<15} + {:<10} + {:<10} + {:<10} + {:<10} + {:<10}  +'.format(product.get_product_code(), product.get_product_name(), product.get_quantity(), product.get_selling_price(), product.get_cost_price(), product.get_manufacture_date(), product.get_expiration_date())
+            print(row)
+        print(row1)
 
     def add_product(self, product_code, product_name, selling_price, cost_price, quantity, manufacture_date, expiration_date):
         product = Product(product_code, product_name, selling_price, cost_price, quantity, manufacture_date, expiration_date)
-        self.product_list.append(product)
+        self.product_add.append(product)
+
+    def search_by_name(self, search_name):
+        found_products = []
+        self.load_products_from_file()
+        for product in self.product_show:
+            if search_name.lower() in product.get_product_name().lower():
+                found_products.append(product)
+        if found_products:
+            print("Kết quả tìm kiếm theo tên:")
+            self.display_products(found_products)
+        else:
+            print("Không tìm thấy sản phẩm phù hợp với tên đã nhập.")
+
+    def search_by_code(self, search_code):
+        found_product = None
+        if found_product:
+            return True
+        else:
+            return False
 
     def display_all_products(self):
-        if len(self.product_list) == 0:
+        self.load_products_from_file()
+        if len(self.product_show) == 0:
             print("Không có hàng hoá nào trong danh sách.")
         else:
             row1 = '+ {:-<5} + {:-<15} + {:-<10} + {:-<10} + {:-<10} + {:-<10} + {:-<10}  +'.format('','','','','','','')
@@ -18,8 +62,7 @@ class ManageProduct:
             print(row1)
             print(row2)
             print(row1)
-            for product in self.product_list:
+            for product in self.product_show:
                 row = '+ {:<5} + {:<15} + {:<10} + {:<10} + {:<10} + {:<10} + {:<10}  +'.format(product.get_product_code(), product.get_product_name(), product.get_quantity(), product.get_selling_price(), product.get_cost_price(), product.get_manufacture_date(), product.get_expiration_date())
                 print(row)
-
             print(row1)
